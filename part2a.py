a0b261776cd2ade18154cee3c8460e52d439d4e9
guthibuserpandae <thatch0nkypig@gmail.com>
2026-02-19 17:54:43 +0000

    part 2 a

---
+++ part2a.py
@@ -0,0 +1,17 @@
+from random import randint
+
+random_list = [randint(1,51) for i in range(10)]
+print(random_list)
+random_search_num = randint(1,20)
+print("random number to search for:", random_search_num)
+
+found = False 
+for number in random_list:
+    if number == random_search_num:
+        found = True
+        break
+if found: 
+    print(str(random_search_num)+" is in the list")
+else: 
+    print(str(random_search_num)+" is not in the list")
+
