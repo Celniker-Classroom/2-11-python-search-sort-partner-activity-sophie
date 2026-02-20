c97d9e8d479bc84701342c1f75bbe4d0f7464112
guthibuserpandae <thatch0nkypig@gmail.com>
2026-02-20 17:21:48 +0000

    joejoejpart3

--- part3.py
+++ part3.py
@@ -3,8 +3,17 @@ from random import randint
 random_list = [randint(1,51) for i in range(10)]
 print(random_list)
 
-max = random_list[0] #start with the first element
-for num in random_list:
-    if num > max:
-        max = num
-print("Maximum:",max)
+max_cnt = 0
+mode = None
+for number in random_list:
+    count = random_list.count(number)
+    if count > max_cnt:
+        max_cnt = count
+        mode = number
+
+# check if a mode exist
+if max_cnt > 1:
+    print("Mode", mode)
+
+else: 
+    print("There is no mode")
